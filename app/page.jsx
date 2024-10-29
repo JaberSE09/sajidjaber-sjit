bbf11c1010f73287412f4371a0fb5fafd3f63ed5
Sajid Jaber <sajidjaber@yahoo.com>
2024-10-29 14:28:52 -0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import Preview from "./preview/page";
+export const metadata = {
+  title: "Krozo - Preview || React Personal Portfolio Template",
+};
+export default function Home() {
+  return <Preview />;
+}
