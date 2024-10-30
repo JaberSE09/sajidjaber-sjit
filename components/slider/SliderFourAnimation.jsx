263f40411ef1c7e935dd37949ddbb3bf9cb027ce
Sajid Jaber <sajidjaber@yahoo.com>
2024-10-30 10:05:39 -0500

    fixed hero

--- components/slider/SliderFourAnimation.jsx
+++ components/slider/SliderFourAnimation.jsx
@@ -3,71 +3,67 @@ import Social from "../Social";
 const Slider = () => {
   return (
     <>
-      {/*  Home Banner */}
+      {/* Home Banner */}
       <section
         id="home"
-        className="home-banner overlay overlay-65 scrollSpysection "
+        className="home-banner overlay overlay-65 scrollSpysection"
         style={{
           backgroundImage: `url(${"/img/banner/fullstack.jpg"})`,
-          backgroundSize: "contain",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       >
-
-        {/* End particle animation */}
-        <div >
-          <div className="row full-screen">
-            <div className="col-lg-12" >
-              <div className="hb-typo text-center bg-[#0E0F11] pt-10">
-                <h6 data-aos="fade-up" data-aos-duration="1200">
-                  Hi There, I'm
-                </h6>
-                <h1
-                  className="font-alt"
-                  data-aos="fade-up"
-                  data-aos-duration="1200"
-                  data-aos-delay="100"
-                >
-                  Sajid Jaber
-                </h1>
-                <div>
-                  <div className="nav ht-list justify-content-center">
-                    <span
-                      className="font-alt"
-                      data-aos="fade-up"
-                      data-aos-duration="1200"
-                      data-aos-delay="150"
-                    >
-                     Full Stack Web Designer
-                    </span>{" "}
-                    <span
-                      className="font-alt"
-                      data-aos="fade-up"
-                      data-aos-duration="1200"
-                      data-aos-delay="200"
-                    >
-                      App Developer
-                    </span>
-                    <span
-                      className="font-alt"
-                      data-aos="fade-up"
-                      data-aos-duration="1200"
-                      data-aos-delay="250"
-                    >
-                      UI/UX Designer
-                    </span>
-                  </div>
-                </div>
-              </div>
+        {/* Adding padding-top to prevent overlap with the navbar */}
+        <div className="h-screen flex flex-col justify-start items-center pt-24 md:pt-36">
+          <div className="w-full max-w-3xl text-center px-6 md:px-12 bg-[#0E0F11]/80 rounded-lg py-8 md:py-10">
+            <h6
+              data-aos="fade-up"
+              data-aos-duration="1200"
+              className="text-base md:text-lg text-gray-200"
+            >
+              Hi There, I'm
+            </h6>
+            <h1
+              className="font-alt text-3xl md:text-5xl font-semibold text-white mt-2 md:mt-4"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+              data-aos-delay="100"
+            >
+              Sajid Jaber
+            </h1>
+            <div className="flex flex-col md:flex-row justify-center mt-6 space-y-2 md:space-y-0 md:space-x-6">
+              <span
+                className="font-alt text-base md:text-lg text-gray-300"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="150"
+              >
+                Full Stack Web Designer
+              </span>
+              <span
+                className="font-alt text-base md:text-lg text-gray-300"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="200"
+              >
+                App Developer
+              </span>
+              <span
+                className="font-alt text-base md:text-lg text-gray-300"
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay="250"
+              >
+                UI/UX Designer
+              </span>
             </div>
           </div>
         </div>
-        {/* End .container */}
-
 
         <Social />
-        {/* End social slide  */}
+        {/* End social slide */}
       </section>
-      {/* End Home Banner  */}
+      {/* End Home Banner */}
     </>
   );
 };
